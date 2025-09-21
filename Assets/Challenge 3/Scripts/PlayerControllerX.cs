22255cd40566253a4804f53cf204dba585ac1822
Nageri Ibrahim <ibrahimdamilola43@gmail.com>
2025-09-21 21:28:29 +0100

    Update PlayerControllerX.cs

--- Assets/Challenge 3/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -36,7 +36,7 @@ public class PlayerControllerX : MonoBehaviour
         // While space is pressed and player is low enough, float up
         if (Input.GetKeyDown(KeyCode.Space) && !gameOver)
         {
-            if (transform.position.y < 13f)
+            if (transform.position.y < 12f)
             {
                 playerRb.AddForce(Vector3.up * floatForce, ForceMode.Impulse);
             }
